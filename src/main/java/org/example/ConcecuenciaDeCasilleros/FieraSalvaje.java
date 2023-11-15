9b0d4f928a0c7c6baa04f955dc027102fc0afbfd
Nazareno <nazarodriguez56@gmail.com>
2023-11-15 18:32:09 -0300

    Elimino metodos de FieraSalvaje que estaban al dope

--- src/main/java/org/example/ConcecuenciaDeCasilleros/FieraSalvaje.java
+++ src/main/java/org/example/ConcecuenciaDeCasilleros/FieraSalvaje.java
@@ -1,8 +1,5 @@
 package org.example.ConcecuenciaDeCasilleros;
 
-import org.example.ConcecuenciaDeCasilleros.Concecuencia;
-import org.example.Equipamiento.Armadura;
-import org.example.Equipamiento.Casco;
 import org.example.Gladiador;
 
 public class FieraSalvaje implements Concecuencia {
@@ -10,11 +7,5 @@ public class FieraSalvaje implements Concecuencia {
     public void afectarGladiador(Gladiador gladiador) {
         // Una fiera aspira a causar 20 de daño, la evasion (la defensa) depende del gladiador
         gladiador.recibirAtaque(this);
-    }
-
-    public void lograrDanioATravesDeEquipamiento(Armadura armadura) {
-    }
-
-    public void lograrDanioATravesDeEquipamiento(Casco armadura) {
     }
 }
